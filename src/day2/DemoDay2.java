8468c93073884727f08fc2073d6f34a496878f61
zackw <zackw@AJ.lan>
2023-07-29 10:34:28 -0400

    Implemented add ()

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -5,6 +5,17 @@ public class DemoDay2 {
 	public static void main(String[] args) {
 		
 		System.out.println("Test");
+		
+		
+	}
+	
+	public static void add (int a, int b) {
+	
+	System.out.println(a+b);
+
 	}
 
-}
+	}
+	
+
+
