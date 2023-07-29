35680a0d04ed2c469f4173eeb18d66e3ac95e042
vsusu571 <140634525+vsusu571@users.noreply.github.com>
2023-07-29 10:55:36 -0400

    Update DemoDay2.java

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -8,14 +8,24 @@ public class DemoDay2 {
 		
 		
 	}
-	
+	//add
 	public static void add (int a, int b) {
 	
 	System.out.println(a+b);
 
 	}
+	//Yasir 
+	public static void divide(int a, int b){
+		System.out.println(a/b);
+	
+	}
+	public static void subtract(int a , int b){
+		System.out.println(a-b);
 
 	}
-	
+	//add method
+	public static void add( double a, int b, int c){
+		System.out.println(a+b+c);
+}
 
 
